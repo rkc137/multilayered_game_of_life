c03c474b5e69bf87228d5d5ffb58f14f827b6ce3
rkc137 <hlopov100@gmail.com>
2025-12-08 06:42:16 +0300

    no i dont gonna use it

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,11 @@
 #include <SFML/Graphics.hpp>
 
+#include "sfml-gui/gui.hpp"
+#include "sfml-gui/widgets/button.hpp"
+#include "sfml-gui/widgets/label.hpp"
+#include "sfml-gui/widgets/checkbox.hpp"
+#include "sfml-gui/widgets/listbox.hpp"
+
 #include <random>
 #include <thread>
 #include <chrono>
@@ -38,8 +44,53 @@ int main()
     window.setFramerateLimit(target_fps);
 
     bool is_introverts = true;
-
+    bool is_pause = false;
     int rule_idx = 0;
+
+
+    /////   ui setup
+    SFGUI::SFMLGUI gui(window, "configuration");
+    SFGUI::Label size_label{""};
+    auto update_size_label = [&](){
+        size_label.setText("past size: " + std::to_string(config.past_size_ref));
+    };
+    update_size_label();
+    auto add_past_size = [&](int delta){
+        //for now its just reassign
+        int psize = config.past_size_ref + delta;
+        if(psize < 1 || psize > 1024)
+            return;
+        config.set_past_size(psize);
+        maps.resize(psize + 1); // TODO: WTF
+        maps_in_order.assign(maps.begin(), maps.end());
+        setup(maps, full_frame);
+        update_size_label();
+    };
+    SFGUI::Button button_past_incr("past size + 1", [&](){ add_past_size(1); });
+    SFGUI::Button button_past_dicr("past size - 1", [&](){ add_past_size(-1); });
+    SFGUI::Listbox listbox(
+        "Rule",
+        0,
+        std::invoke([&](){
+            std::vector<std::string> variants;
+            for(size_t i = 0; i < Rules::extraverts.size(); i++)
+                variants.emplace_back("extravert " + std::to_string(i));
+            for(size_t i = 0; i < Rules::introverts.size(); i++)
+                variants.emplace_back("introvert " + std::to_string(i));
+            return variants;
+        }),
+        [&](int indx){
+            is_introverts = indx + 1 > static_cast<int>(Rules::extraverts.size());
+            rule_idx = indx - is_introverts * Rules::extraverts.size();
+        }
+    );
+    SFGUI::Checkbox checkbox("Pause", &is_pause);
+    // gui.Add(checkbox);
+    gui.Add(size_label);
+    gui.Add(button_past_incr);
+    gui.Add(button_past_dicr);
+    gui.Add(listbox);
+
     auto update_title = [&](){
         window.setTitle(
             "past size: " + std::to_string(config.past_size_ref) +
@@ -73,18 +124,6 @@ int main()
 
                 switch(k->code)
                 {
-                case Key::Enter:
-                    if(is_ctrl_down)
-                    {
-                        is_introverts ^= true;
-                        rule_idx = 0;
-                    } 
-                    else
-                    {
-                        rule_idx = ++rule_idx % (is_introverts ? Rules::introverts.size() : Rules::extraverts.size());
-                    }
-                    update_title();
-                break;
                 case Key::U:
                     if(++config.premitiva == prims.end())
                         config.premitiva = prims.begin();
@@ -111,23 +150,7 @@ int main()
                 break;
                 case Key::Grave:
                 {
-                    //for now its just reassign
-                    int psize = config.past_size_ref;
-                    if(is_ctrl_down)
-                    {
-                        if(psize <= 1)
-                            break;
-                        psize--;
-                    }
-                    else
-                    {
-                        psize++;
-                    }
-                    config.set_past_size(psize);
-                    maps.resize(psize + 1);
-                    maps_in_order.assign(maps.begin(), maps.end());
-                    setup(maps, full_frame);
-                    update_title();
+                    
                 }
                 break;
                 default:
@@ -141,13 +164,22 @@ int main()
             maps_in_order.begin()
         );
 
-        if(is_introverts)
-            sim_frame(maps_in_order, Rules::introverts[rule_idx]);
-        else
-            sim_frame(maps_in_order, Rules::extraverts[rule_idx]);
-
-        if(!(turn % (howmh_frames_skip + 1)))
+        if(!is_pause)
+        {
+            if(is_introverts)
+                sim_frame(maps_in_order, Rules::introverts[rule_idx]);
+            else
+                sim_frame(maps_in_order, Rules::extraverts[rule_idx]);
+        }
+            
+            if(!(turn % (howmh_frames_skip + 1)))
             draw(window, maps_in_order);
+        
+        gui.Update();
+        gui.Draw();
+        // if (button.isClicked())
+        //     label.setText("Just clicked that button!"); 
+
         window.display();
     }
 
